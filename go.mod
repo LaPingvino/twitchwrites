034e2cc46caede6263f68ceed63474c6c4c18aa9
Joop Kiefte <ikojba@gmail.com>
2019-10-25 18:37:57 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/LaPingvino/twitchwrites
+
+go 1.13
+
+require github.com/fluffle/goirc v1.0.1
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"os"
+	"regexp"
+	"time"
+	"unicode"
+
+	irc "github.com/fluffle/goirc/client"
+)
+
+var regex = "^[^ ]$"
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Printf("%#v", os.Args)
+		panic("Cannot login without username/password and channel")
+	}
+	if len(os.Args) > 4 {
+		regex = os.Args[4]
+	}
+	// Or, create a config and fiddle with it first:
+	cfg := irc.NewConfig(os.Args[1])
+	cfg.SSL = true
+	cfg.SSLConfig = &tls.Config{ServerName: "irc.chat.twitch.tv"}
+	cfg.Server = "irc.chat.twitch.tv:6697"
+	cfg.Pass = os.Args[2]
+	c := irc.Client(cfg)
+
+	// Add handlers to do things here!
+	// e.g. join a channel on connect.
+	c.HandleFunc(irc.CONNECTED,
+		func(conn *irc.Conn, line *irc.Line) { conn.Join(os.Args[3]) })
+	// And a signal on disconnect
+	quit := make(chan bool)
+	c.HandleFunc(irc.DISCONNECTED,
+		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+	// Tell client to connect.
+	if err := c.Connect(); err != nil {
+		fmt.Printf("Connection error: %s\n", err.Error())
+	}
+
+	var timer = time.Tick(15 * time.Minute)
+	var measure map[string]int
+
+	c.HandleFunc(irc.PRIVMSG,
+		func(conn *irc.Conn, line *irc.Line) {
+			if ok, _ := regexp.MatchString(regex, line.Text()); ok {
+				if measure == nil {
+					measure = make(map[string]int)
+				}
+				select {
+				case <-timer:
+					var highest int
+					var chosen string
+					for k, v := range measure {
+						if v < highest {
+							chosen = k
+							highest = v
+						}
+					}
+					space := " "
+					if !unicode.IsLetter([]rune(chosen)[0]) {
+						space = ""
+					}
+					fmt.Print(space + chosen)
+				default:
+					measure[line.Text()]++
+				}
+			}
+		})
+
+	// Wait for disconnect
+	<-quit
+}
